839bb7dbcaf3a31ccaebe8d07387438416e1e9f5
Tom Svensson <tom_svensson@outlook.com>
2025-03-21 12:59:00 +0100

    Add project files.

---
+++ BusinessPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
